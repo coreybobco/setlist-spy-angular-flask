8642676fed40a3f5ef949d997da2bab9931110e5
Corey Bobco <corey.bobco@gmail.com>
2016-12-02 13:14:45 -0600

    moved markov mutagen python classes into subfolder

---
+++ server/mutagen/gene.py
@@ -0,0 +1,28 @@
+from capturer import CaptureOutput
+from gutenberg.acquire import load_etext
+from gutenberg.cleanup import strip_headers
+from internetarchive import download
+import requests
+
+class Gene:
+    def __init__(self, url):
+        return
+
+    def as_dict(self):
+        return ***REMOVED***
+            'document_id': self.document_id,
+            'title': self.title,
+            'author': self.author,
+            'source': self.source,
+            'url': self.url
+        ***REMOVED***
+
+def get_text(gene):
+    if gene['source'] == "Project Gutenberg":
+        text = strip_headers(load_etext(gene['document_id']).strip())
+    elif gene['source'] == "Archive.org":
+        with CaptureOutput() as capturer:
+            download(gene['document_id'], glob_pattern="*txt", dry_run=True)
+        url = capturer.get_text()
+        text = requests.get(url).text
+    return text
---
+++ server/mutagen/textgen.py
@@ -0,0 +1,30 @@
+import markovify
+import json
+from markovify.chain import Chain
+
+class TextGen:
+    def __init__(self):
+        self.markov_models = list()
+        return
+
+    def addMarkov(self, text, options):
+        print("adding Markov")
+        ngram_size = int(options['ngram_size'])
+        self.markov_models.append(markovify.Text(text, state_size=ngram_size))
+
+    def generateText(self):
+        output = "    "
+        if len(self.markov_models) > 1:
+            textgen = markovify.combine(self.markov_models)
+        else:
+            textgen = self.markov_models[0]
+        sentence_count = 0
+        while sentence_count <= 12:
+            sentence = textgen.make_sentence()
+            if isinstance(sentence, str):
+                output += " " + sentence
+                sentence_count += 1
+                if sentence_count % 4 == 0:
+                    print(sentence)
+                    output += "\n\n"
+        return output
